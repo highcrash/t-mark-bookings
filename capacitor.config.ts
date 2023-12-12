3a2b7138b730423f677cdf5eb2e8ba1d5f59b484
highcrash <highcrashweb@gmail.com>
2023-12-12 00:58:47 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'T-Mark Bookings',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
